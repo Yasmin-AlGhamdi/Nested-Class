c1c8113b04718aaf61a3012d6ae51788f660986f
Yasmin-AlGhamdi <82459882+Yasmin-AlGhamdi@users.noreply.github.com>
2021-04-28 22:39:11 +0300

    Add files via upload

---
+++ NestedClass/NestedClass/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace NestedClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal a = new Animal();
+
+            Animal.Cat c = new Animal.Cat();
+            c.print();
+
+
+            Console.ReadKey();
+        }
+    }
+    class Animal
+    {
+        private static string voice = "Sommething";
+        public class Cat
+        {
+            public void print()
+            {
+               // Console.WriteLine("CAT CLASS");
+                Console.WriteLine(Animal.voice="Meow!");               
+            }
+        }
+    }
+}
